3e3fdf54053f48372ef78ac2c811f6f2fd3a9f4d
kevchh89 <47668597+kevchh89@users.noreply.github.com>
2020-05-12 23:20:54 -0400

    Fixed

--- plot2.R
+++ plot2.R
@@ -1,28 +1,23 @@
-rm(list = ls())
 library(lubridate)
-dayss<-c(rep('Sun',24),rep('Mon',24),rep('Tue',24),rep('Wed',24),rep('Thu',24),rep('Fri',24),rep('Sat',24))
-hourss<-rep(0:23,7)
-dypp<-paste(dayss,hourss)
 DT<-read.delim(file.choose(),sep = ";")
-DT$Global_active_power<-as.numeric(DT$Global_active_power)
-DT$Date<-as.Date(DT$Date)
-DT$Date<-wday(DT$Date,label=T)
-DT$Time<-hms(as.character(DT$Time))
-DT$Time<-hour(DT$Time)
-library(dplyr)
-DT2<-DT %>%
-  group_by(Date, Time) %>% 
-  summarise_at(vars("Global_active_power"), mean)
-DayHour<-paste(DT2$Date,DT2$Time)
-DF<-as.data.frame(list(DH=DayHour,Kw=DT2$Global_active_power/1000))
-DF$DH<-factor(DF$DH,levels = dypp)
-
-rowloc<-c(grep('Thu',DF$DH),grep('Fri',DF$DH))
+DT$Global_intensity <- as.Date(DT$Date, format="%d/%m/%Y")
+DT$Global_active_power <- as.numeric(DT$Global_active_power)
+DT$Global_reactive_power <- as.numeric(DT$Global_reactive_power)
+DT$Voltage <- as.numeric(DT$Voltage)
+DT$Global_intensity <- as.numeric(DT$Global_intensity)
+DT$Sub_metering_1 <- as.numeric(DT$Sub_metering_1)
+DT$Sub_metering_2 <- as.numeric(DT$Sub_metering_2)
+DT$Sub_metering_3 <- as.numeric(DT$Sub_metering_3)
+DT2<-subset(DT, Global_intensity == "2007-02-01" | Date =="2007-02-02")
+FullTimeDate <- paste(DT2$Date, DT2$Time, sep=" ")
+FullTimeDate<-dmy_hms(FullTimeDate)
+DT3 <- cbind(DT2, FullTimeDate)
 
 png('plot2.png',height = 480,width = 480)
 {
-  plot(as.numeric(DF$DH)[rowloc],DT2$Global_active_power[rowloc], type='l',xaxt='none',xlab = "",ylab = 'Global Active Power (kilowatts)')
-axis(1, seq(97,145,24),las=1,labels = c('Thu','Fri','Sat'))
+  plot(as.numeric(DT3$FullTimeDate),DT3$Global_active_power, type='l',xaxt='none', xlab = "",ylab = 'Global Active Power (kilowatts)',
+       xlim = c(as.numeric(dmy_hms('01/02/2007 00:00:00')),as.numeric(dmy_hms('02/02/2007 0:00:00'))))
+axis(1, seq(min(as.numeric(DT3$FullTimeDate)),max(as.numeric(DT3$FullTimeDate)),43200),las=1,labels = c('Thu','Fri'))
 }
 dev.off()
 
--- plot4.R
+++ plot4.R
@@ -1,31 +1,17 @@
-rm(list = ls())
 library(lubridate)
-dayss<-c(rep('Sun',24),rep('Mon',24),rep('Tue',24),rep('Wed',24),rep('Thu',24),rep('Fri',24),rep('Sat',24))
-hourss<-rep(0:23,7)
-dypp<-paste(dayss,hourss)
 DT<-read.delim(file.choose(),sep = ";")
-DT$Voltage<-as.numeric(DT$Voltage)
-DT$Global_active_power<-as.numeric(DT$Global_active_power)
-DT$Global_reactive_power<-as.numeric(DT$Global_reactive_power)
-DT$Sub_metering_1<-as.numeric(DT$Sub_metering_1)
-DT$Sub_metering_2<-as.numeric(DT$Sub_metering_2)
-DT$Sub_metering_3<-as.numeric(DT$Sub_metering_3)
-DT$Date<-as.Date(DT$Date)
-
-DT$Date<-wday(DT$Date,label=T)
-DT$Time<-hms(as.character(DT$Time))
-DT$Time<-hour(DT$Time)
-library(dplyr)
-DT2<-DT %>%
-  group_by(Date, Time) %>% 
-  summarise_all(mean,na.rm=T)
-DayHour<-paste(DT2$Date,DT2$Time)
-DF<-as.data.frame(list(DH=DayHour,GAP=DT2$Global_active_power,
-                       GRP=DT2$Global_reactive_power,VOL=DT2$Voltage,SM1=DT2$Sub_metering_1,
-                       SM2=DT2$Sub_metering_2,SM3=DT2$Sub_metering_3))
-DF$DH<-factor(DF$DH,levels = dypp)
-
-rowloc<-c(grep('Thu',DF$DH),grep('Fri',DF$DH))
+DT$Global_intensity <- as.Date(DT$Date, format="%d/%m/%Y")
+DT$Global_active_power <- as.numeric(DT$Global_active_power)
+DT$Global_reactive_power <- as.numeric(DT$Global_reactive_power)
+DT$Voltage <- as.numeric(DT$Voltage)
+DT$Global_intensity <- as.numeric(DT$Global_intensity)
+DT$Sub_metering_1 <- as.numeric(DT$Sub_metering_1)
+DT$Sub_metering_2 <- as.numeric(DT$Sub_metering_2)
+DT$Sub_metering_3 <- as.numeric(DT$Sub_metering_3)
+DT2<-subset(DT, Global_intensity == "2007-02-01" | Date =="2007-02-02")
+FullTimeDate <- paste(DT2$Date, DT2$Time, sep=" ")
+FullTimeDate<-dmy_hms(FullTimeDate)
+DT3 <- cbind(DT2, FullTimeDate)
 
 png('plot4.png',height = 480,width = 480)
 
@@ -34,26 +20,26 @@ png('plot4.png',height = 480,width = 480)
 par(mfrow=c(2,2),mar=c(4,4,2,2))
 {
   ### 1
-  plot(as.numeric(DF$DH)[rowloc],DT2$Global_active_power[rowloc], 
+  plot(as.numeric(DT3$FullTimeDate),DT3$Global_active_power, 
        type='l',xaxt='none',xlab = "",ylab = 'Global Active Power')
-  axis(1, seq(97,145,24),las=1,labels = c('Thu','Fri','Sat'))
+  axis(1, seq(min(as.numeric(DT3$FullTimeDate)),max(as.numeric(DT3$FullTimeDate)),43200),las=1,labels = c('Thu','Fri'))
   ### 2
-  plot(as.numeric(DF$DH)[rowloc],DT2$Voltage[rowloc], 
+  plot(as.numeric(DT3$FullTimeDate),DT3$Voltage, 
        type='l',xaxt='none',xlab = "datetime",ylab = 'Voltage')
-  axis(1, seq(97,145,24),las=1,labels = c('Thu','Fri','Sat'))
+  axis(1, seq(min(as.numeric(DT3$FullTimeDate)),max(as.numeric(DT3$FullTimeDate)),43200),las=1,labels = c('Thu','Fri'))
   ### 3
-  plot(as.numeric(DF$DH)[rowloc],DT2$Sub_metering_1[rowloc], type='l',xaxt='none',xlab = "",
+  plot(as.numeric(DT3$FullTimeDate),DT3$Sub_metering_1, type='l',xaxt='none',xlab = "",
        ylab = 'Energy sub metering',ylim = c(1,50))
-  axis(1, seq(97,145,24),las=1,labels = c('Thu','Fri','Sat'))
-  lines(as.numeric(DF$DH)[rowloc],DT$Sub_metering_2[rowloc],col='red')
-  lines(as.numeric(DF$DH)[rowloc],DT$Sub_metering_3[rowloc],col='blue')
+  axis(1, seq(min(as.numeric(DT3$FullTimeDate)),max(as.numeric(DT3$FullTimeDate)),43200),las=1,labels = c('Thu','Fri'))
+  lines(as.numeric(DT3$FullTimeDate),DT3$Sub_metering_2,col='red')
+  lines(as.numeric(DT3$FullTimeDate),DT3$Sub_metering_3,col='blue')
   legend('topright',95,legend=c('Sub_metering_1','Sub_metering_1','Sub_metering_3'),
          col = c('black','red','blue'),lty = c(1,1,1),cex=0.2)
   ### 4
-  plot(as.numeric(DF$DH)[rowloc],DT2$Global_reactive_power[rowloc], 
+  plot(as.numeric(DT3$FullTimeDate),DT3$Global_reactive_power, 
        type='l',xaxt='none',xlab = "datetime",
        ylab = 'Global Reactive Power')
-  axis(1, seq(97,145,24),las=1,labels = c('Thu','Fri','Sat'))
+  axis(1, seq(min(as.numeric(DT3$FullTimeDate)),max(as.numeric(DT3$FullTimeDate)),43200),las=1,labels = c('Thu','Fri'))
 }
 
 dev.off()
